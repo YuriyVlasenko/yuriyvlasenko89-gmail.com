4b93a538bb6240b6f5ed667c6e52331929f31a80
Yurii Vlasenko <yuriyvlasenko89@gmail.com>
2020-05-07 23:00:49 +0300

    remove reload on 401

--- src/app/interceptors/errorInterceptor.ts
+++ src/app/interceptors/errorInterceptor.ts
@@ -23,7 +23,6 @@ export class ErrorInterceptor implements HttpInterceptor {
         if (err.status === 401) {
           // auto logout if 401 response returned from api
           this.loginService.signOut();
-          location.reload(true);
         }
 
         const error = err.error.message || err.statusText;
